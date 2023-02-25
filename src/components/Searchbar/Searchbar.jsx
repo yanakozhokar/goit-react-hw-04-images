f862053eca69f0a548848cd281a81a0b339c44d7
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-25 20:49:09 +0200

    searchbar updated

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,59 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import css from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    filter: '',
+const Searchbar = ({ onSubmit }) => {
+  const [filter, setFilter] = useState('');
+
+  const inputHandler = event => {
+    const { value } = event.currentTarget;
+    setFilter(value);
   };
 
-  inputHandler = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  submitHandler = event => {
+  const submitHandler = event => {
     event.preventDefault();
-    if (!this.state.filter.trim()) {
+    if (!filter.trim()) {
       Notify.failure('Fill in the input line');
       return;
     }
 
-    this.props.onSubmit(this.state.filter);
-    this.reset();
+    onSubmit(filter);
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      filter: '',
-    });
+  const reset = () => {
+    setFilter('');
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.submitHandler}>
-          <button type="submit" className={css.SearchFormButton}>
-            <ImSearch size={20} />
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            name="filter"
-            value={this.state.filter}
-            onChange={this.inputHandler}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={submitHandler}>
+        <button type="submit" className={css.SearchFormButton}>
+          <ImSearch size={20} />
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={inputHandler}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
